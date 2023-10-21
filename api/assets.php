15dc120a9e8665e5507ffa6ffb6cadb0b4fcb267
Matt Goldsworthy <matt.j.goldsworthy@gmail.com>
2023-10-21 15:26:08 -0600

    working with vercel

--- api/assets.php
+++ api/assets.php
@@ -2,9 +2,11 @@
 
 $file = $_GET['assets'];
 if (str_ends_with($file, '.css')) {
-    header("Content-type: text/css; charset: UTF-8");
-    return require '/var/task/user/public/build/assets/' . basename($file);
-} else if (str_ends_with($file, '.js')) {
+    header('Content-type: text/css; charset: UTF-8');
+
+    return require dirname(__DIR__).'/public/build/assets/'.basename($file);
+} elseif (str_ends_with($file, '.js')) {
     header('Content-Type: application/javascript; charset: UTF-8');
-    return require '/var/task/user/public/build/assets/' . basename($file);
+
+    return require dirname(__DIR__).'/public/build/assets/'.basename($file);
 }
